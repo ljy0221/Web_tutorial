98050eb6770a7576b2d9559febfbbb362bcf7145
ljy0221 <jaeyeong221@gmail.com>
2025-03-31 18:28:47 +0900

    Add week11

---
+++ week11/practice-1/controllers/todos.controller.js
@@ -0,0 +1,67 @@
+const Todo = require("../models/todo.model");
+
+async function getAllTodos(req, res, next) {
+  let todos;
+  try {
+    todos = await Todo.getAllTodos();
+  } catch (error) {
+    return next(error);
+  }
+
+  res.json({
+    todos: todos,
+  });
+}
+
+async function addTodo(req, res, next) {
+  const todoText = req.body.text;
+
+  const todo = new Todo(todoText);
+
+  let insertedId;
+  try {
+    const result = await todo.save();
+    insertedId = result.insertedId;
+  } catch (error) {
+    return next(error);
+  }
+
+  todo.id = insertedId.toString();
+  res.json({ message: "Added todo successfully!", createdTodo: todo });
+}
+
+async function updateTodo(req, res, next) {
+  const todoId = req.params.id;
+  const newText = req.body.newText;
+
+  const todo = new Todo(newText, todoId);
+
+  try {
+    await todo.save();
+  } catch (error) {
+    return next(error);
+  }
+
+  res.json({ message: "Todo updated", updateTodo: todo });
+}
+
+async function deleteTodo(req, res, next) {
+  const todoId = req.params.id;
+
+  const todo = new Todo(null, todoId);
+
+  try {
+    await todo.delete();
+  } catch (error) {
+    return next(error);
+  }
+
+  res.json({ message: "Todo deleted", updateTodo: todo });
+}
+
+module.exports = {
+  getAllTodos: getAllTodos,
+  addTodo: addTodo,
+  updateTodo: updateTodo,
+  deleteTodo: deleteTodo,
+};
